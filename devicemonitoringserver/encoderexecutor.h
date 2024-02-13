bb83ada938c225be7b4931dfe17b5904364df5d0
SergeyMezentsev <mr.sm2002@mail.ru>
2024-02-13 23:33:47 +0500

    add messsageEncoder-class and write virtual destructor to the baseEncoderEzecutor-class

--- devicemonitoringserver/encoderexecutor.h
+++ devicemonitoringserver/encoderexecutor.h
@@ -12,37 +12,42 @@ public:
     virtual std::string encode (const std::string&) = 0;
     virtual std::string decode (const std::string&) = 0;
     std::string name() const {return _name;}
+    virtual ~baseEncoderExecutor(){}
 };
 
 
 class ROT3 final : public baseEncoderExecutor {
 public:
     ROT3() {_name = "ROT3";}
-    std::string encode (const std::string&) ;
-    std::string decode (const std::string&) ;
+    std::string encode (const std::string&);
+    std::string decode (const std::string&);
+    ~ROT3() override {}
 };
 
 
 class Mirror final : public baseEncoderExecutor {
 public:
     Mirror() {_name = "Mirror";}
-    std::string encode (const std::string&) ;
-    std::string decode (const std::string&) ;
+    std::string encode (const std::string&);
+    std::string decode (const std::string&);
+    ~Mirror() override {}
 };
 
 
 class Multiply41 final : public baseEncoderExecutor {
 public:
     Multiply41() {_name = "Multiply41";}
-    std::string encode (const std::string&) ;
-    std::string decode (const std::string&) ;
+    std::string encode (const std::string&);
+    std::string decode (const std::string&);
+    ~Multiply41() override {}
 };
 
 class Multiply5 final : public baseEncoderExecutor {
 public:
     Multiply5() {_name = "Multiply5";}
-    std::string encode (const std::string&) ;
-    std::string decode (const std::string&) ;
+    std::string encode (const std::string&);
+    std::string decode (const std::string&);
+    ~Multiply5() override {}
 };
 
 #endif //ENCODEREXECUTOR_H
